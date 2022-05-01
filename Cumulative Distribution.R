b728c3ca50085bfe9d562e90f8c7dccc309a0ea8
Göker Malik Altuntaş <gmalikaltuntas@gmail.com>
2022-05-01 11:05:04 +0300

    Create Cumulative Distribution.R

---
+++ Cumulative Distribution.R
@@ -0,0 +1,28 @@
+#Check if the required packages were installed and attached
+packsAsked <- c("dslabs")
+
+insDemandIndex <- !(packsAsked %in% rownames(installed.packages()))
+sapply(packsAsked[insDemandIndex], install.packages)
+
+attDemandIndex <- !(packsAsked %in% .packages())
+packsToAtt <- packsAsked[attDemandIndex]
+
+for (i in packsToAtt){
+  library(i, character.only = TRUE)
+}
+
+
+#start coding here
+
+#function to calculate the percentage of a single pin
+cumulative_distribution <- function(pin) {
+  mean(heights$height <= pin)
+}
+
+# define range of 100 values from lowest value to heighest value
+hist_pins <- seq(min(heights$height), max(heights$height), length = 100)
+
+# applt the function over all the pins
+pin_values <- sapply(hist_pins, cumulative_distribution)
+
+plot(hist_pins, pin_values)
