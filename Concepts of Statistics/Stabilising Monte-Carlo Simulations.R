2ff98a94cbbc97a0887500289a87bcd03cb75e9c
Göker Malik Altuntaş <gmalikaltuntas@gmail.com>
2022-05-06 15:43:27 +0300

    Create Stabilising Monte-Carlo Simulations.R

---
+++ Concepts of Statistics/Stabilising Monte-Carlo Simulations.R
@@ -0,0 +1,37 @@
+#Check if the required packages were installed and attached
+packsAsked <- c("gtools", "tidyverse")
+
+insDemandIndex <- !(packsAsked %in% rownames(installed.packages()))
+sapply(packsAsked[insDemandIndex], install.packages)
+
+attDemandIndex <- !(packsAsked %in% .packages())
+packsToAtt <- packsAsked[attDemandIndex]
+
+for (i in packsToAtt){
+  library(i, character.only = TRUE)
+}
+
+#start coding here
+
+#configure the RNG to ensure the interoperability with the course material
+set.seed(1)
+signif(3)
+
+
+
+# Course 3 
+#1.2 Combinations and Permutations
+#4- How Many Monte Carlo Experiments are Enough?
+
+B <- 10^seq(1, 5, len = 100)    # defines vector of many B values
+compute_prob <- function(B, n = 30){    # function to run Monte Carlo simulation with each B
+  same_day <- replicate(B, {
+    bdays <- sample(1:365, n, replace = TRUE)
+    any(duplicated(bdays))
+  })
+  mean(same_day)
+}
+
+prob <- sapply(B, compute_prob)    # apply compute_prob to many values of B
+plot(log10(B), prob, type = "l")    # plot a line graph of estimates 
+
