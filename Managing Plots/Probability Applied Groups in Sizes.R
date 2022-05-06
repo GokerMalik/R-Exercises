1602d43b550fc56cea41c78033a2bd81c73eef4f
Göker Malik Altuntaş <gmalikaltuntas@gmail.com>
2022-05-06 14:29:45 +0300

    Update Probability Applied Groups in Sizes.R

--- Managing Plots/Probability Applied Groups in Sizes.R
+++ Managing Plots/Probability Applied Groups in Sizes.R
@@ -1,5 +1,5 @@
 #Check if the required packages were installed and attached
-packsAsked <- c("gtools")
+packsAsked <- c()
 
 insDemandIndex <- !(packsAsked %in% rownames(installed.packages()))
 sapply(packsAsked[insDemandIndex], install.packages)
@@ -18,32 +18,34 @@ set.seed(1986, sample.kind = "Rounding")
 
 # Course 3 
 #1.2 Combinations and Permutations
-#2- sapply
+#3- sapply
 
-#write a function that calculates the probability of at least two people
+#define groups from with one member to 60 members
+n <- seq(1,60)
+
+#write a function that runs Monte-Carlo to estimate the probability of at least two people
 #having the same birthday for a given number of group
-compute_prob <- function (n, B = 10000){
-  same_day <- replicate(B, {
+MonteCarlo <- function (n, count = 10000){
+  same_day <- replicate(count, {
     bdays <- sample(1:365, n, replace = TRUE)
     any(duplicated(bdays))
   })
  mean(same_day)
 }
 
-#define groups from with one member to 60 members
-n <- seq(1,60)
-
 #apply the function over each. Line temporarily commented to avoid time lost
-#prob <- sapply(n, compute_prob)
-
-#plot the results vs number of group members
-#plot(n, prob)
+probMC <- sapply(n, MonteCarlo)
 
 #define the same function without the monte-carlo experiment
-compute_probWMC <- function (n){
-  prob_unique <- seq(365, 365-n+1)/365
+compute_prob <- function (Arg){
+  #probability of NOT having a birthday for a new member of n membered group
+  #goes (365/365)*(364/365)*(363/365)*(362/365)....
+  prob_unique <- seq(365, 365-Arg)/365
   1-prod(prob_unique)
 }
 
 prob <- sapply(n, compute_prob)
+
+#plot MonteCarlo over calculations
+plot(n, probMC)
 lines(n, prob, col = "red")
