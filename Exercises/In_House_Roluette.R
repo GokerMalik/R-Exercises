58519fba812a0e805233dcbcf141e5f2740c8588
Göker Malik Altuntaş <gmalikaltuntas@gmail.com>
2022-05-22 03:40:22 +0300

    22.05.22

---
+++ Exercises/In_House_Roluette.R
@@ -0,0 +1,20 @@
+#In House Roluette chance of losing money for 500 bets
+
+set.seed(21, sample.kind = "Rounding")
+
+price <- 6
+penalty <- -1
+
+p_win = 5/38
+p_loose <- 1-p_win
+
+expected_one_bet <- 6*p_win -1*p_loose
+se_one_bet <- abs(price-penalty)*sqrt(p_win*p_loose)
+
+a500_bets_avg <- expected_one_bet
+se_500_bets_avg <- se_one_bet/sqrt(500)
+
+expected_500_bets <- expected_one_bet * 500
+se_500_bets <- se_one_bet*sqrt(500)
+
+pnorm(0, expected_500_bets, se_500_bets)
